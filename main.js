a88801c8234ec7d63b46a6bfc2d40f0efaeb2773
EliasHaynes <eliasmhaynes@gmail.com>
2022-12-07 20:09:12 -0600

    added button for each user to display additional information

--- main.js
+++ main.js
@@ -23,20 +23,49 @@ function getTwentyUsers() {
             console.log('Get Users:', arrayOfUsers)
         })
 }
+const allUsers = document.getElementById('all-users')
+// const li = document.createElement('li');
+// li.id = 'button'
 
 const displayUser =() => {
-    const allUsers = document.getElementById('all-users')
+    // const allUsers = document.getElementById('all-users')
     //arrayOfUsers itself is an object because we turned it into JSON but the array results is nested in the object and contains all the data so then we direct into that array so we can then perform Array higher order functions on the data. If we dont direct to the results array and try to do Array functions on the entire JSON then we will get not a function because you cant perfrom array fucntions on an object!
     arrayOfUsers.results.map((user) => {
+        //display their names
         const li = document.createElement('li');
-        
+        li.id = 'button'
+        const text = document.createTextNode(`First Name: ${user.name.first}, Last Name: ${user.name.last},`)
+        //display their thumbnails
         const source = user.picture.thumbnail
         const img = document.createElement('img')
         img.src = source;
-        const text = document.createTextNode(`First Name: ${user.name.first}, Last Name: ${user.name.last},`)
+        //display more info buttons
+        const butt = document.createElement('button')
+        const buttDisplayText = document.createTextNode("More Info")
+        butt.onclick = displayMoreUserInfo
+        //button
+        butt.appendChild(buttDisplayText)
+        allUsers.append(butt)
+        //li text
         li.appendChild(text)
         allUsers.append(li)
+        //img
         allUsers.append(img)
+        
     })
+}
+
+const displayMoreUserInfo = () => {
+    arrayOfUsers.results.map((user) => {
+        const grabAge = user.dob.age
+        const grabPhone = user.phone
+        let liId = document.getElementById('button')
+        
+        const theInfo = document.createTextNode(` Age: ${grabAge} Phone #: ${grabPhone}`)
+        liId.appendChild(theInfo)
+        allUsers.append(li)
+    })
+    console.log('Hey!')
+    
 }
 
