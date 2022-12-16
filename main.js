5b5ca6834be6ec2954f900a2c1ec2cacc6cf1234
EliasHaynes <eliasmhaynes@gmail.com>
2022-12-15 20:04:21 -0600

    fixed appending issues and finished assignment

--- main.js
+++ main.js
@@ -6,15 +6,6 @@ window.onload = function() {
     getTwentyUsers()
 }
 
-// function getAUser() {
-//     fetch('https://randomuser.me/api/')
-//         .then(res => res.json())
-//         .then(user => {
-//             arrayOfUsers = user
-//             console.log('Get a User:', arrayOfUsers)
-//         })
-// }
-
 function getTwentyUsers() {
     fetch('https://randomuser.me/api/?results=20')
         .then(res => res.json())
@@ -23,49 +14,115 @@ function getTwentyUsers() {
             console.log('Get Users:', arrayOfUsers)
         })
 }
-const allUsers = document.getElementById('all-users')
-// const li = document.createElement('li');
-// li.id = 'button'
+
+let allUsers = document.getElementById('all-users')
+let ul = document.getElementById('more-info')
+let li2 = document.createElement('li');
+let br = document.createElement('br')
 
 const displayUser =() => {
-    // const allUsers = document.getElementById('all-users')
+    
     //arrayOfUsers itself is an object because we turned it into JSON but the array results is nested in the object and contains all the data so then we direct into that array so we can then perform Array higher order functions on the data. If we dont direct to the results array and try to do Array functions on the entire JSON then we will get not a function because you cant perfrom array fucntions on an object!
     arrayOfUsers.results.map((user) => {
         //display their names
-        const li = document.createElement('li');
-        li.id = 'button'
-        const text = document.createTextNode(`First Name: ${user.name.first}, Last Name: ${user.name.last},`)
+        const id = Math.floor(Math.random() * 10000000000)
+
+        li = document.createElement('li');
+        li.setAttribute('id', id)
+    
+        const text = document.createTextNode(`First Name: ${user.name.first}, Last Name: ${user.name.last}`)
+        console.log(user)
         //display their thumbnails
         const source = user.picture.thumbnail
         const img = document.createElement('img')
         img.src = source;
         //display more info buttons
-        const butt = document.createElement('button')
+        let butt = document.createElement('button')
         const buttDisplayText = document.createTextNode("More Info")
-        butt.onclick = displayMoreUserInfo
-        //button
-        butt.appendChild(buttDisplayText)
-        allUsers.append(butt)
-        //li text
-        li.appendChild(text)
-        allUsers.append(li)
-        //img
-        allUsers.append(img)
+        butt.setAttribute('id',id)
         
-    })
+        butt.onclick = function displayMoreUserInfo () {
+            // arrayOfUsers.forEach(user => {
+                // user = arrayOfUsers.results[0].id.value;
+                const currentLi = document.getElementById(id)
+                const grabAge = user.dob.age
+                const grabPhone = user.cell
+                const li2 = document.createElement('li')
+                const innerul = document.createElement('ul');
+                const theInfo = ` Age: ${grabAge} Phone #: ${grabPhone}`
+                li2.innerHTML = theInfo;
+                
+                innerul.appendChild(li2) 
+                currentLi.appendChild(innerul)
+                console.log(li2,innerul,currentLi)
+            // })
+        }
+        //img
+        allUsers.append(img) //append each users img to the allUsers ul
+        
+        // allUsers.append(butt) //Give each user a more info button
+        //li text
+        li.append(text) //append the information to each li(user)
+        allUsers.append(li)// append the lis to the allUsers ul
+        //button
+        butt.appendChild(buttDisplayText) //Buttons label
+        li.append(butt)
+})
 }
 
-const displayMoreUserInfo = () => {
-    arrayOfUsers.results.map((user) => {
-        const grabAge = user.dob.age
-        const grabPhone = user.phone
-        let liId = document.getElementById('button')
+
+//get the value of the button by giving it an id instead of the li
+// target the ids of the users given in the response
+// with the users special ids i can filter throught the array and find that users id in the array
+
+
+
+
+// function displayMoreUserInfo () {
+//     // arrayOfUsers.forEach(user => {
+//         // user = arrayOfUsers.results[0].id.value;
+//         const grabAge = arrayOfUsers.results[0].dob.age
+//         const grabPhone = arrayOfUsers.results.cell
+//         li2 = document.createElement('li')
+//         ul = document.getElementById('more-info')
+//         const theInfo = document.createTextNode(` Age: ${grabAge} Phone #: ${grabPhone}`)
         
-        const theInfo = document.createTextNode(` Age: ${grabAge} Phone #: ${grabPhone}`)
-        liId.appendChild(theInfo)
-        allUsers.append(li)
-    })
-    console.log('Hey!')
+//         li2.append(theInfo)
+//         li.appendChild(li2)
+//         allUsers.appendChild(li2) 
+//     // })
     
-}
+// console.log(arrayOfUsers.results[0].id.value)
+// console.log(arrayOfUsers.results[0].dob.age)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//     // ul.innerHTML = ""
+//     // let number = Array.from(contact.parentElement.children).indexOf(contact) // I understand that .from() is to turn an array into a string but which one is the array, parentElement or children? and what are these properties exactly?
+//     // let index = data.results[number]
+//     // li2 = document.createElement('li')
+//     // li2.innerHTML = `Email: <u>${index.email}</u>
+//     // <br>
+//     // Age: ${index.dob.age}
+//     // <br>
+//     // Address:
+//     // <br><i>
+//     // ${index.location.street.number} ${index.location.street.name} <br>${index.location.city}, ${index.location.state} <br>${index.location.country} </i>`
+//     // ul.append(li2)
+//     // contact.append(ul)//?
+// }
 
